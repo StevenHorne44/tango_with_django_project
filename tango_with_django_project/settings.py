63531bfcadc70f70a584cf7d564dcebb5bee8688
Steven Horne <3005574H@student.gla.ac.uk>
2026-02-09 16:00:22 +0000

    Chapter 9 before exercises

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -29,6 +29,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = 'rango:login'
+
 
 # Application definition
 
@@ -101,7 +103,17 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
+}
 ]
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
 
 
 # Internationalization
